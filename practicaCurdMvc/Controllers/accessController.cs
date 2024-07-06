30da13c30551743771a2d233ea3e85173c06f6d3
Bantrax947 <francofabian947@gmail.com>
2024-07-06 13:59:17 -0400

    Agregar archivos de proyecto.

---
+++ practicaCurdMvc/Controllers/accessController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace practicaCurdMvc.Controllers
+{
+    public class accessController : Controller
+    {
+        // GET: access
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Enter(String user, String Password)
+        {
+            try
+            {
+                return Content("1");
+
+            }
+            catch (Exception ex)
+            {
+
+                return Content("Ocurrio un error" + ex.Message);
+            }
+        }
+    }
+}
